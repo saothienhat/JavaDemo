e784f3f6c4d205a40035408c134d182942328018
GLOBAL\I347371 <binh.trinh@sap.com>
2018-06-06 15:57:44 +0700

    Update code for Read/Write file in Java

--- JavaCore/javacore/src/main/java/saothienhat/javacore/file/ReadWriteFileHandler.java
+++ JavaCore/javacore/src/main/java/saothienhat/javacore/file/ReadWriteFileHandler.java
@@ -1,9 +1,41 @@
 package saothienhat.javacore.file;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author Sao Thien Hat
  *
  */
 public class ReadWriteFileHandler {
+  
+  public ReadWriteFileHandler() {
+    
+  }
+
+  public void readFileByBufferedReader(String fileName) {
+    BufferedReader br = null;
+    FileReader fr = null;
+    try {
+        fr = new FileReader(fileName);
+        br = new BufferedReader(fr);
+        String currentLine = "";
+        while ((currentLine = br.readLine()) != null) {
+            System.out.println(currentLine);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (br != null)
+                br.close();
+            if (fr != null)
+                fr.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+  }
 
 }
